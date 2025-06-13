d8c55282d06db844f1617765192503003616bd0a
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-06-13 16:23:49 -0500

    feat: add Backend and Frontend folders from branches

---
+++ Backend/src/test/java/com/afk/rentasmart/RentaSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afk.rentasmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
