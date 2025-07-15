20e712f69a2dedaabd531398884ee5260583a121
san ats <san@gmail.com>
2025-07-14 19:57:07 -0500

    Configuracion del Entorno Inicial

---
+++ Frontend/src/routes/AppsRouter.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+
+
+export const AppsRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+    </Routes>
+  );
+};
