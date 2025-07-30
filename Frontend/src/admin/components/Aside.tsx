6e45be02f6b47a596598b2ce6d453ec42e82cf07
san ats <san@gmail.com>
2025-07-29 23:04:54 -0500

    Aside mejorado y responsive

--- Frontend/src/admin/components/Aside.tsx
+++ Frontend/src/admin/components/Aside.tsx
@@ -26,48 +26,48 @@ export const Aside = () => {
 
 
     return (
-        <div className="w-[15%] h-full flex flex-col items-start justify-start border-r border-[#C7C6BA] gap-2 p-5">
-            <div className="w-full h-[50px] bg-[#FEFCEC] flex flex-row items-center mb-2 p-2">
+        <div className="w-auto h-full flex flex-col items-start justify-start border-r border-[#C7C6BA] gap-2 p-5 max-[1082px]:p-2">
+            <div className="w-full h-[50px] bg-[#FEFCEC] flex flex-row items-center justify-center mb-2 p-2 max-[1082px]:justify-start">
                 <img src={imgLogo} alt="Logo" className="w-[37.33px] h-[50px]"/>
-                <h1 className="text-[#393939] text-[20px] font-semibold">Renta<span className="text-[#EB8369] text-[20px] font-semibold">Smart</span></h1>
+                <h1 className="text-[#393939] xl:text-[20px] font-semibold max-[1082px]:hidden">Renta<span className="text-[#EB8369] xl:text-[20px] font-semibold max-[1082px]:hidden">Smart</span></h1>
             </div>
             <div className="w-full h-full flex flex-col items-start justify-center gap-5 p-2">
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isHome ? imgHomeOn : imgHomeOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin"><span className={isHome ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Inicio</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin"><img src={isHome ? imgHomeOn : imgHomeOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin"><span className={isHome ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Inicio</span></Link>
                     <div className={isHome ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isPublications ? imgPubliOn : imgPubliOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin/publications"><span className={isPublications ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Publicaciones</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin/publications"><img src={isPublications ? imgPubliOn : imgPubliOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin/publications"><span className={isPublications ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Publicaciones</span></Link>
                     <div className={isPublications ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isContracts ? imgContratosOn : imgContratosOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin/contracts"><span className={isContracts ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Contratos</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin/contracts"><img src={isContracts ? imgContratosOn : imgContratosOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin/contracts"><span className={isContracts ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Contratos</span></Link>
                     <div className={isContracts ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isMessages ? imgChatOn : imgChatOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin/messages"><span className={isMessages ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Mensajes</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin/messages"><img src={isMessages ? imgChatOn : imgChatOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin/messages"><span className={isMessages ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Mensajes</span></Link>
                     <div className={isMessages ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isServices ? imgServicioOn : imgServicioOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin/services"><span className={isServices ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Servicios</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin/services"><img src={isServices ? imgServicioOn : imgServicioOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin/services"><span className={isServices ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Servicios</span></Link>
                     <div className={isServices ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
             </div>
 
             <div className="w-full h-full flex flex-col items-start justify-end gap-5 p-2">
-                <div className="flex flex-row items-center justify-between w-full">
-                    <img src={isUser ? imgUserOn : imgUserOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <Link to="/admin/user"><span className={isUser ? "text-[#EB8369] text-[14px] font-semibold" : "text-[#A9ADB6] text-[14px] font-semibold"}>Usuario</span></Link>
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
+                    <Link to="/admin/user"><img src={isUser ? imgUserOn : imgUserOff} alt="Logo" className="w-[30px] h-[30px]"/></Link>
+                    <Link to="/admin/user"><span className={isUser ? "text-[#EB8369] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden" : "text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden"}>Usuario</span></Link>
                     <div className={isUser ? "w-2 aspect-square rounded-full bg-[#EB8369]" : ""}></div>
                 </div>
-                <div className="flex flex-row items-center justify-between w-full">
+                <div className="flex flex-row items-center justify-between w-full max-[1082px]:justify-start max-[1082px]:gap-2">
                     <img src={imgExitOff} alt="Logo" className="w-[30px] h-[30px]"/>
-                    <span className="text-[#A9ADB6] text-[14px] font-semibold">Salir</span>
+                    <span className="text-[#A9ADB6] xl:text-[14px] text-[12px] font-semibold max-[1082px]:hidden">Salir</span>
                     <div className=""></div>
                 </div>
             </div>
