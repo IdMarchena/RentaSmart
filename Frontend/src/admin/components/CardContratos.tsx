a87f59771307cc0a9d6ab73448661ef59114e59b
san ats <san@gmail.com>
2025-07-31 21:49:11 -0500

    ContratosDash terminado con su responsive

---
+++ Frontend/src/admin/components/CardContratos.tsx
@@ -0,0 +1,34 @@
+import imgHistory from "../../assets/history.png"
+import imgVista from "../../assets/vista.png"
+import imgEditar from "../../assets/editar.png"
+import imgBorrar from "../../assets/borrar.png"
+import imgFolder from "../../assets/folder.png"
+
+export const CardContratos = () => {
+    return (
+        <div className="w-full h-[105px] flex flex-row items-start justify-between bg-[#E1DFD2] border-[1px] border-[#BCBBB0] rounded-[10px] p-2">
+            <div className="w-[75%] flex flex-row items-start justify-start gap-2 ml-2">
+                <img src={imgFolder} alt="add" className="w-[30px] h-[30px] object-cover rounded-[20px] mt-2"/>
+                <div className="w-[70%] flex flex-col items-start justify-center gap-1 mt-2">
+                    <h1 className="text-[#393939] text-[12px] font-bold">Contrato - Apt 123</h1>
+                    <p className="text-[#393939] text-[9px] font-medium">Contrato de alquiler de apartamento ubicado en una zona tranquila y de fácil acceso. Cuenta con excelente iluminación natural...</p>
+                </div>
+            </div>
+            <div className="w-[25%] flex flex-col items-start justify-end gap-1 flex-nowrap mr-2 mt-1">
+                <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap">
+                    <img src={imgHistory} alt="history" className="w-[15px] h-[15px] object-cover"/>
+                    <span className="text-[#393939] text-[8px] font-medium">20 ene 2024</span>
+
+                </div>
+                <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap mt-9">
+                    <img src={imgVista} alt="vista" className="w-[25px] h-[25px] object-cover"/>
+                    <img src={imgEditar} alt="editar" className="w-[25px] h-[25px] object-cover"/>
+                    <img src={imgBorrar} alt="borrar" className="w-[25px] h-[25px] object-cover"/>
+
+                </div>
+            </div>
+           
+            
+        </div>
+    )
+}
--- Frontend/src/admin/pages/ContratoDash.tsx
+++ Frontend/src/admin/pages/ContratoDash.tsx
@@ -1,12 +1,31 @@
-
 import { Aside } from "../components/Aside"
-
+import imgAdd1 from "../../assets/add-1.png"
+import { CardContratos } from "../components/CardContratos"
 export const ContratoDash = () => {
     return (
         <div className="w-full h-screen flex flex-row">
             <Aside />
-            <div className="w-[85%] h-full flex flex-col items-center justify-center">
-                <h1>ContratoDash</h1>
+            <div className="w-[85%] h-full flex flex-col items-start justify-start p-5 overflow-y-scroll custom-scrollbar-2 max-[1082px]:w-[100%]">
+                <div className="w-full h-[100px] flex flex-row items-center justify-between mt-3">
+                        <h1 className="text-[#393939] text-[20px] font-bold"> Tus Contratos</h1>
+                        <div className="flex flex-row items-center gap-1 rounded-[10px] bg-[#EB8369] p-2 shadow-[0px_10px_10px_rgba(0,0,0,0.2)]">
+                            <img src={imgAdd1} alt="" className="w-[20px] h-[20px] object-cover" />
+                            <span className="text-[#FFFFFF] text-[12px] font-bold">Crear</span>
+                        </div>
+                </div>
+                <div className="w-full h-[100vh] flex flex-row items-center justify-start gap-10 mt-5 max-[890px]:flex-col max-[890px]:h-[200vh]">
+                    <div className="w-[50%] h-[100vh] flex flex-col items-start justify-start gap-5 bg-[#EFEDDE] border-[1px] border-[#BCBBB0] rounded-[10px] overflow-y-scroll custom-scrollbar-1 overflow-x-hidden p-5 max-[890px]:w-full max-[890px]:h-[100vh]">
+                        <CardContratos />
+                        <CardContratos />
+                        <CardContratos />
+                        <CardContratos />
+                        <CardContratos />
+                    </div>
+                    <div className="w-[50%] h-[100vh] flex flex-col items-center justify-center gap-5 bg-[#EFEDDE] border-[1px] border-[#BCBBB0] rounded-[10px] overflow-x-hidden p-5 max-[890px]:w-full">
+                        <span className="text-[#393939] text-[12px] font-bold">Vista Previa del Contrato</span>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
