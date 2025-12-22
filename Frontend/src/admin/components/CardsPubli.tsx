b1229b3a0470cad5ab314ed467e16f8bd8bfb0f4
san ats <san@gmail.com>
2025-12-22 10:33:47 -0500

    Carta de creacion para publicacion creada

--- Frontend/src/admin/components/CardsPubli.tsx
+++ Frontend/src/admin/components/CardsPubli.tsx
@@ -3,32 +3,75 @@ import imgB1 from "../../assets/b1.jpg"
 import imgVista from "../../assets/vista.png"
 import imgEditar from "../../assets/editar.png"
 import imgBorrar from "../../assets/borrar.png"
+import { CardEditPubli } from "./CardEditPubli"
+import {useState} from "react";
+import { CardConfirmation} from "@/admin/components/CardConfirmation.tsx";
 
 export const CardsPubli = () => {
+    const [OpenPublicModal, setOpenPublicModal] = useState(false);
+    const [OpenConfirmationModal, setOpenConfirmationModal] = useState(false);
+
+    const togglePublicModal = () => {
+        setOpenPublicModal(!OpenPublicModal);
+    };
+
+    const toggleConfirmationModal = () => {
+        setOpenConfirmationModal(!OpenConfirmationModal);
+    }
     return (
-        <div className="w-full h-[105px] flex flex-row items-start justify-between bg-[#E1DFD2] border-[1px] border-[#BCBBB0] rounded-[10px] p-2">
-            <div className="w-[75%] flex flex-row items-start justify-start gap-2 ml-2">
-                <img src={imgB1} alt="add" className="w-[50%] h-[90px] object-cover rounded-[20px]"/>
-                <div className="w-[70%] flex flex-col items-start justify-center gap-1 mt-2">
-                    <h1 className="text-[#393939] text-[12px] font-bold">Apartamento</h1>
-                    <p className="text-[#393939] text-[9px] font-medium">Moderno apartamento de 2 habitaciones ubicado en una zona tranquila y de fácil acceso. Cuenta con excelente iluminación natural...</p>
+        <>
+            <div className="w-full h-[105px] flex flex-row items-start justify-between bg-[#E1DFD2] border-[1px] border-[#BCBBB0] rounded-[10px] p-2">
+                <div className="w-[75%] flex flex-row items-start justify-start gap-2 ml-2">
+                    <img src={imgB1} alt="add" className="w-[50%] h-[90px] object-cover rounded-[20px]"/>
+                    <div className="w-[70%] flex flex-col items-start justify-center gap-1 mt-2">
+                        <h1 className="text-[#393939] text-[12px] font-bold">Apartamento</h1>
+                        <p className="text-[#393939] text-[9px] font-medium">Moderno apartamento de 2 habitaciones ubicado en una zona tranquila y de fácil acceso. Cuenta con excelente iluminación natural...</p>
+                    </div>
+                </div>
+                <div className="w-[25%] flex flex-col items-start justify-end gap-1 flex-nowrap mr-2 mt-1">
+                    <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap">
+                        <img src={imgHistory} alt="history" className="w-[15px] h-[15px] object-cover"/>
+                        <span className="text-[#393939] text-[8px] font-medium">20 ene 2024</span>
+
+                    </div>
+                    <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap mt-9">
+                        <img src={imgVista} alt="vista" className="w-[25px] h-[25px] object-cover"/>
+                        <img src={imgEditar} alt="editar" className="w-[25px] h-[25px] object-cover cursor-pointer" onClick={togglePublicModal} />
+                        <img src={imgBorrar} alt="borrar" className="w-[25px] h-[25px] object-cover cursor-pointer" onClick={toggleConfirmationModal} />
+
+                    </div>
                 </div>
             </div>
-            <div className="w-[25%] flex flex-col items-start justify-end gap-1 flex-nowrap mr-2 mt-1">
-                <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap">
-                    <img src={imgHistory} alt="history" className="w-[15px] h-[15px] object-cover"/>
-                    <span className="text-[#393939] text-[8px] font-medium">20 ene 2024</span>
+            {OpenPublicModal && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    {/* Overlay opaco */}
+                    <div
+                        className="absolute inset-0  bg-opacity-50 backdrop-blur-sm"
+                        onClick={togglePublicModal}
+                    ></div>
 
+                    {/* Card de Login */}
+                    <div className="relative z-10">
+                        <CardEditPubli onClose={togglePublicModal} />
+                    </div>
                 </div>
-                <div className="w-full flex flex-row items-start justify-end gap-1 flex-nowrap mt-9">
-                    <img src={imgVista} alt="vista" className="w-[25px] h-[25px] object-cover"/>
-                    <img src={imgEditar} alt="editar" className="w-[25px] h-[25px] object-cover"/>
-                    <img src={imgBorrar} alt="borrar" className="w-[25px] h-[25px] object-cover"/>
+            )}
+            {OpenConfirmationModal && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    {/* Overlay opaco */}
+                    <div
+                        className="absolute inset-0  bg-opacity-50 backdrop-blur-sm"
+                        onClick={toggleConfirmationModal}
+                    ></div>
 
+                    {/* Card de Confirmacion */}
+                    <div className="relative z-10">
+                        <CardConfirmation onClose={toggleConfirmationModal} />
+                    </div>
                 </div>
-            </div>
-           
-            
-        </div>
+            )}
+
+        </>
+
     )
 }
--- Frontend/src/admin/pages/PublicationDash.tsx
+++ Frontend/src/admin/pages/PublicationDash.tsx
@@ -3,35 +3,61 @@ import imgAdd1 from "../../assets/add-1.png"
 import { CardChart2 } from "../components/CardChart-2"
 import { CardChart3 } from "../components/CardChart-3"
 import { CardsPubli } from "../components/CardsPubli"
+import { CardCreatePubli} from "@/admin/components/CardCreatePubli.tsx";
+import {useState} from "react";
+
 
 export const PublicationDash = () => {
+    const [OpenPublicModal, setOpenPublicModal] = useState(false);
+
+    const togglePublicModal = () => {
+        setOpenPublicModal(!OpenPublicModal);
+    };
+
     return (
-        <div className="w-full h-screen flex flex-row">
-            <Aside />
-            <div className="w-[85%] h-full flex flex-col items-start justify-start p-5 overflow-y-scroll custom-scrollbar-2 max-[1082px]:w-[100%]">
-                <div className="w-full h-[100px] flex flex-row items-center justify-between mt-3 cursor-pointer">
-                    <h1 className="text-[#393939] text-[20px] font-bold"> Tus Publicaciones</h1>
-                    <div className="flex flex-row items-center gap-1 rounded-[10px] bg-[#EB8369] p-2 shadow-[0px_10px_10px_rgba(0,0,0,0.2)]">
-                        <img src={imgAdd1} alt="" className="w-[20px] h-[20px] object-cover" />
-                        <span className="text-[#FFFFFF] text-[12px] font-bold">Publicar</span>
+        <>
+            <div className="w-full h-screen flex flex-row">
+                <Aside />
+                <div className="w-[85%] h-full flex flex-col items-start justify-start p-5 overflow-y-scroll custom-scrollbar-2 max-[1082px]:w-[100%]">
+                    <div className="w-full h-[100px] flex flex-row items-center justify-between mt-3 cursor-pointer">
+                        <h1 className="text-[#393939] text-[20px] font-bold"> Tus Publicaciones</h1>
+                        <div className="flex flex-row items-center gap-1 rounded-[10px] bg-[#EB8369] p-2 shadow-[0px_10px_10px_rgba(0,0,0,0.2)] cursor-pointer" onClick={togglePublicModal}>
+                            <img src={imgAdd1} alt="" className="w-[20px] h-[20px] object-cover" />
+                            <span className="text-[#FFFFFF] text-[12px] font-bold">Publicar</span>
+                        </div>
                     </div>
-                </div>
-                <div className="w-full h-[400px] flex flex-row items-center justify-start gap-5 mt-5 max-[890px]:flex-col max-[890px]:h-auto">
-                    <div className="w-[60%] h-[400px] flex flex-col items-start justify-start gap-5 bg-[#EFEDDE] border-[1px] border-[#BCBBB0] rounded-[10px] overflow-y-scroll custom-scrollbar-1 overflow-x-hidden p-5 max-[890px]:w-full max-[890px]:h-[400px]">
-                        <CardsPubli />
-                        <CardsPubli />
-                        <CardsPubli />
-                        <CardsPubli />
-                        <CardsPubli />
-                        
+                    <div className="w-full h-[400px] flex flex-row items-center justify-start gap-5 mt-5 max-[890px]:flex-col max-[890px]:h-auto">
+                        <div className="w-[60%] h-[400px] flex flex-col items-start justify-start gap-5 bg-[#EFEDDE] border-[1px] border-[#BCBBB0] rounded-[10px] overflow-y-scroll custom-scrollbar-1 overflow-x-hidden p-5 max-[890px]:w-full max-[890px]:h-[400px]">
+                            <CardsPubli />
+                            <CardsPubli />
+                            <CardsPubli />
+                            <CardsPubli />
+                            <CardsPubli />
+
+                        </div>
+                        <CardChart2 />
                     </div>
-                    <CardChart2 />
+                    <div className="w-full h-[400px] flex flex-row items-center justify-start gap-5 mt-5">
+                        <CardChart3 />
+                    </div>
+
                 </div>
-                <div className="w-full h-[400px] flex flex-row items-center justify-start gap-5 mt-5">
-                    <CardChart3 />
-                </div>
-                
             </div>
-        </div>
+            {OpenPublicModal && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    {/* Overlay opaco */}
+                    <div
+                        className="absolute inset-0  bg-opacity-50 backdrop-blur-sm"
+                        onClick={togglePublicModal}
+                    ></div>
+
+                    {/* Card de public */}
+                    <div className="relative z-10">
+                        <CardCreatePubli onClose={togglePublicModal} />
+                    </div>
+                </div>
+            )}
+        </>
+
     )
 }
