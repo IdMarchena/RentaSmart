36c01979a32308da0f7109e655e79bdfb83f9771
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-12-23 14:57:56 -0500

    refactor(service): adjust requirement service logic

--- Backend/src/main/java/com/afk/control/service/impl/RequisitoServiceImpl.java
+++ Backend/src/main/java/com/afk/control/service/impl/RequisitoServiceImpl.java
@@ -3,6 +3,7 @@ package com.afk.control.service.impl;
 import com.afk.control.dto.RequisitoDto;
 import com.afk.control.mapper.RequisitoMapper;
 import com.afk.control.service.RequisitoService;
+import com.afk.model.entity.Requisito;
 import com.afk.model.repository.RequisitoRepository;
 import com.afk.model.repository.TipoRequisitoRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,22 +12,69 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RequisitoServiceImpl implements RequisitoService {
 
     private final RequisitoRepository requisitoRepository;
-    private final TipoRequisitoRepository tipoRequisitoRepository;
     @Qualifier("requisitoMapperImpl")
     private final RequisitoMapper mapper;
-    
     @Override
-    RequisitoDto createRequisito(RequisitoDto requisito);
-    
+    public RequisitoDto createRequisito(RequisitoDto requisito) {
+        if (requisito == null) {
+            throw new IllegalArgumentException("El requisito no puede ser nulo");
+        }
+        Requisito requi = mapper.toEntity(requisito);
+        return Optional.ofNullable(requi)
+                .map(requisitoRepository::save)
+                .map(savedRequi -> mapper.toDto(savedRequi))
+                .orElseThrow(() -> new RuntimeException("Creación de requisito inválida"));
+    }
     @Override
-    @
-    RequisitoDto findRequisitoById(Long id);
-    List<RequisitoDto> findAllRequisitos();
-    void deleteRequisitoById(Long id);
+    @Transactional(readOnly = true)
+    public RequisitoDto findRequisitoById(Long id){
+        if(id == null || id <= 0){
+            throw new IllegalArgumentException("El id del requisito no puede ser nulo o negativo");
+        }
+        Requisito requisito = requisitoRepository.findById(id)
+                        .orElseThrow(() -> new NoSuchElementException("Requisito con id " + id + " no encontrado"));
+        return mapper.toDto(requisito);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<RequisitoDto> findAllRequisitos(){
+        List<Requisito> requisitos = requisitoRepository.findAll();
+        if(requisitos.isEmpty()){
+            throw new NoSuchElementException("Requisitos no encontradas");
+        }
+        return mapper.toDtoList(requisitos);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public void deleteRequisitoById(Long id){
+        if(id == null || id <= 0){
+            throw new NoSuchElementException("Requisito no encontrado");
+        }
+        requisitoRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public void updateRequisito(RequisitoDto requisito) {
+        if(requisito == null){
+            throw new IllegalArgumentException("El requisito no puede ser nulo");
+        }
+
+        Requisito requisitoEntity = mapper.toEntity(requisito);
+
+        Requisito rExistente = requisitoRepository.findById(requisitoEntity.getId())
+                .orElseThrow(() -> new IllegalArgumentException("requisito no encontrado"));
+
+        rExistente.setDescripcion(requisitoEntity.getDescripcion());
+        rExistente.setTipo(requisitoEntity.getTipo());
+        requisitoRepository.save(rExistente);
+    }
 }
