07468cee7f6bc70941426f934fa3775d009d9f46
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-12-27 12:31:12 -0500

    feat: add Servicio and TipoServicio controllers and services

---
+++ Backend/src/main/java/com/afk/control/controller/ServicioController.java
@@ -0,0 +1,177 @@
+package com.afk.control.controller;
+
+import com.afk.control.dto.ServicioDto;
+import com.afk.control.service.ServicioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.afk.control.dto.JsonResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servicios")
+@RequiredArgsConstructor
+public class ServicioController {
+
+    private final ServicioService service;
+
+    @PostMapping("/crear")
+    public ResponseEntity<JsonResponse<ServicioDto>> crearServicio(@RequestBody ServicioDto servicio) {
+        ServicioDto servicioDto = service.crearServicio(servicio);
+        return ResponseEntity.ok(
+                new JsonResponse<>(
+                        true,
+                        "Servicio creado exitosamente",
+                        servicioDto,
+                        201
+                )
+        );
+    }
+
+    @GetMapping("7obtenerPorId/{id}")
+    public ResponseEntity<JsonResponse<ServicioDto>> getServicioById(@PathVariable Long id) {
+        ServicioDto servicioDto = service.getServicioById(id);
+        if (servicioDto != null) {
+            return ResponseEntity.ok(
+                    new JsonResponse<>(
+                            true,
+                            "Servicio encontrado",
+                            servicioDto,
+                            200
+                    )
+            );
+        } else {
+            return ResponseEntity.status(404).body(
+                    new JsonResponse<>(
+                            false,
+                            "Servicio no encontrado",
+                            null,
+                            404
+                    )
+            );
+        }
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<JsonResponse<List<ServicioDto>>> getAllServicios() {
+        List<ServicioDto> servicios = service.getAllServicios();
+        return ResponseEntity.ok(
+                new JsonResponse<>(
+                        true,
+                        "Servicios obtenidos correctamente",
+                        servicios,
+                        200
+                )
+        );
+    }
+
+    @DeleteMapping("/eliminarPorId/{id}")
+    public ResponseEntity<JsonResponse<Void>> eliminarServicio(@PathVariable Long id) {
+        try {
+            service.eliminarServicio(id);
+            return ResponseEntity.ok(
+                    new JsonResponse<>(
+                            true,
+                            "Servicio eliminado exitosamente",
+                            null,
+                            200
+                    )
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(
+                    new JsonResponse<>(
+                            false,
+                            "Servicio no encontrado para eliminar",
+                            null,
+                            404
+                    )
+            );
+        }
+    }
+
+    @PutMapping("/actualizarPorId/{id}")
+    public ResponseEntity<JsonResponse<ServicioDto>> actualizarServicio(@PathVariable Long id, @RequestBody ServicioDto servicio) {
+        try{
+            service.actualizarServicio(id, servicio);
+            return ResponseEntity.ok(
+                    new JsonResponse<>(
+                            true,
+                            "Servicio actualizado exitosamente",
+                            null,
+                            200
+                    )
+            );
+        }catch (Exception e) {
+            return ResponseEntity.status(404).body(
+                    new JsonResponse<>(
+                            false,
+                            "Servicio no encontrado para actualizar",
+                            null,
+                            404
+                    )
+            );
+        }
+    }
+    @GetMapping("/buscarServiciosPorNombre/{nombre}")
+    public ResponseEntity<JsonResponse<List<ServicioDto>>> buscarServicioPorNombre(@PathVariable String nombre) {
+        List<ServicioDto> servicios = service.buscarServicioPorNombre(nombre);
+        return ResponseEntity.ok(
+                new JsonResponse<>(
+                        true,
+                        "Servicios encontrados por nombre",
+                        servicios,
+                        200
+                )
+        );
+    }
+    @GetMapping("/buscarServiciosPorTipo/{tipo}")
+    public ResponseEntity<JsonResponse<List<ServicioDto>>> buscarServiciosPorTipo(@PathVariable String tipo) {
+        List<ServicioDto> servicios = service.buscarServiciosPorTipo(tipo);
+        return ResponseEntity.ok(
+                new JsonResponse<>(
+                        true,
+                        "Servicios encontrados por tipo",
+                        servicios,
+                        200
+                )
+        );
+    }
+    @GetMapping("/buscarServiciosPorNombreYPrecios")
+    public ResponseEntity<JsonResponse<List<ServicioDto>>> buscarServiciosPorNombreYPrecio(@RequestParam String nombre, @RequestParam Integer precio) {
+        List<ServicioDto> servicios = service.buscarServiciosPorNombreYPrecio(nombre, precio);
+        return ResponseEntity.ok(
+                new JsonResponse<>(
+                        true,
+                        "Servicios encontrados por nombre y precio",
+                        servicios,
+                        200
+                )
+        );
+    }
+
+    @PutMapping("/cambiarEstadoServicioPorId/{id}")
+    public ResponseEntity<JsonResponse<Void>> cambiarEstadoServicioPorIdServicio(@PathVariable Long id, @RequestParam String estado) {
+        try{
+            service.cambiarEstadoServicioPorIdServicio(id, estado);
+            return ResponseEntity.ok(
+                    new JsonResponse<>(
+                            true,
+                            "Estado de servicio actualizado exitosamente",
+                            null,
+                            200
+                    )
+            );
+        }catch (Exception e) {
+            return ResponseEntity.status(404).body(
+                    new JsonResponse<>(
+                            false,
+                            "Servicio no encontrado para cambiar el estado",
+                            null,
+                            404
+                    )
+            );
+        }
+    }
+}
---
+++ Backend/src/main/java/com/afk/control/service/impl/ServicioServiceImpl.java
@@ -0,0 +1,123 @@
+package com.afk.control.service.impl;
+import com.afk.control.dto.ServicioDto;
+import com.afk.control.mapper.ServicioMapper;
+import com.afk.control.service.ServicioService;
+import com.afk.model.entity.Servicio;
+import com.afk.model.entity.enums.EstadoServicio;
+import com.afk.model.repository.ServicioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ServicioServiceImpl implements ServicioService {
+    private final ServicioRepository repository;
+
+    private final ServicioMapper mapper;
+
+
+    @Override
+    public ServicioDto crearServicio(ServicioDto servicio) {
+        if(servicio==null) throw new IllegalArgumentException("El servicio no puede ser nulo");
+        Servicio s = mapper.toEntity(servicio);
+        return mapper.toDto(repository.save(s));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ServicioDto getServicioById(Long id) {
+        if(id==null || id < 0) throw new IllegalArgumentException("El id no puede ser nulo o menor a  0");
+        Servicio s = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("El servicio no existe"));
+        return mapper.toDto(s);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ServicioDto> getAllServicios() {
+        List<Servicio> s = repository.findAll();
+        if (s.isEmpty()) throw new NoSuchElementException("El servicio no existe");
+        return mapper.toDtoList(s);
+    }
+
+    @Override
+    public void eliminarServicio(Long id) {
+        if(id==null || id < 0) throw new IllegalArgumentException("El id no puede ser nulo o menor a  0");
+        Servicio s = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("El servicio no existe"));
+        repository.delete(s);
+    }
+
+    @Override
+    public void actualizarServicio(Long id,ServicioDto servicio) {
+        if(servicio==null) throw new IllegalArgumentException("El servicio no puede ser nulo");
+        Servicio s = mapper.toEntity(servicio);
+        Servicio sExistente = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("El servicio no existe"));
+        sExistente.setNombre(s.getNombre());
+        sExistente.setDescripcion(s.getDescripcion());
+        sExistente.setUsuario(s.getUsuario());
+        sExistente.setTipo(s.getTipo());
+        sExistente.setPrecio(s.getPrecio());
+        sExistente.setEstado(s.getEstado());
+        repository.save(sExistente);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ServicioDto> buscarServicioPorNombre(String nombre) {
+        List<Servicio> lista = repository.findAll();
+        if (lista.isEmpty()) throw new NoSuchElementException("El servicio no existe");
+        List<Servicio> listaFiltrada = lista.stream()
+                .filter(s -> s.getNombre().equals(nombre))
+                .collect(Collectors.toList());
+        return mapper.toDtoList(listaFiltrada);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ServicioDto> buscarServiciosPorTipo(String tipo) {
+        EstadoServicio estado = switch (tipo) {
+            case "SOLICITADO" -> EstadoServicio.SOLICITADO;
+            case "APROBADO" -> EstadoServicio.APROBADO;
+            case "PENDIENTE" -> EstadoServicio.PENDIENTE;
+            default -> throw new NoSuchElementException("El servicio no existe");
+        };
+        List<Servicio> listaFiltrada = repository.findByEstado(estado);
+        if (listaFiltrada.isEmpty()) {
+            throw new NoSuchElementException("No se encontraron servicios con el tipo: " + tipo);
+        }
+        return mapper.toDtoList(listaFiltrada);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ServicioDto> buscarServiciosPorNombreYPrecio(String nombre, Integer precio) {
+        if(nombre == null || precio <0) throw new IllegalArgumentException("El nombre no puede ser negativo");
+        List<Servicio> lista = repository.findByNombre(nombre);
+        if (lista.isEmpty()) throw new NoSuchElementException("El servicio no existe");
+        List<Servicio> listaFiltrada = lista.stream()
+                .filter(s -> s.getPrecio() <= precio)
+                .collect(Collectors.toList());
+        return mapper.toDtoList(listaFiltrada);
+    }
+
+    @Override
+    public void cambiarEstadoServicioPorIdServicio(Long id, String estado) {
+        if(id <0 || estado == null) throw new IllegalArgumentException("El id no puede ser negativo");
+        Servicio s = repository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("El servicio no existe"));
+        EstadoServicio e = switch (estado) {
+            case "SOLICITADO" -> EstadoServicio.SOLICITADO;
+            case "APROBADO" -> EstadoServicio.APROBADO;
+            case "PENDIENTE" -> EstadoServicio.PENDIENTE;
+            default -> throw new NoSuchElementException("El servicio no existe");
+        };
+        s.setEstado(e);
+        repository.save(s);
+    }
+}
