e64b1f6f747981fc3c0a4d1f2800ec4d06e8c615
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-12-29 14:18:42 -0500

    chore: remove non-essential modules to simplify core domain

--- Backend/src/main/java/com/afk/model/entity/enums/EstadoReporteFinanciero.java
+++
@@ -1,5 +0,0 @@
-package com.afk.model.entity.enums;
-
-public enum EstadoReporteFinanciero {
-    ACTIVA, VENCIDA, RESUELTA, ELIMINADA
-}
--- Backend/src/main/java/com/afk/model/entity/enums/EstadoReporteMantenimiento.java
+++
@@ -1,5 +0,0 @@
-package com.afk.model.entity.enums;
-
-public enum EstadoReporteMantenimiento {
-    ACTIVA, VENCIDA, RESUELTA, ELIMINADA
-}
