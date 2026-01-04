8fea37c9982a4538448b233794764ec9ebbf7376
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2026-01-04 11:47:40 -0500

    build: adjust dependencies and application bootstrap

--- Backend/src/main/java/com/afk/rentasmart/RentaSmartApplication.java
+++ Backend/src/main/java/com/afk/rentasmart/RentaSmartApplication.java
@@ -2,8 +2,15 @@ package com.afk.rentasmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.afk.client",
+        "com.afk.control",
+        "com.afk.model",
+        "com.afk.bootstrap"
+})
 public class RentaSmartApplication {
 
     public static void main(String[] args) {
