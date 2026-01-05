7cbaa4b2dd158626ef5915cda7997934be8f2081
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2026-01-05 03:59:53 -0500

    build: update project configuration and bootstrap

--- Backend/src/main/java/com/afk/rentasmart/RentaSmartApplication.java
+++ Backend/src/main/java/com/afk/rentasmart/RentaSmartApplication.java
@@ -2,8 +2,12 @@ package com.afk.rentasmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.afk.model.repository") // Asegúrate de que esta ruta sea exacta
+@EntityScan(basePackages = "com.afk.model.entity")
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.afk.client",
